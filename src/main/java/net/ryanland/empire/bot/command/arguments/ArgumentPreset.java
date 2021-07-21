b086c81b845484f425075919ae29a4c390915847
RyanLandDev <ryanlandofficial@hotmail.com>
2021-07-21 03:19:12 +0200

    Added USER and MEMBER ArgumentPreset

--- src/main/java/net/ryanland/empire/bot/command/arguments/ArgumentPreset.java
+++ src/main/java/net/ryanland/empire/bot/command/arguments/ArgumentPreset.java
@@ -1,4 +1,17 @@
 package net.ryanland.empire.bot.command.arguments;
 
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+import net.ryanland.empire.bot.command.arguments.types.impl.MemberArgument;
+import net.ryanland.empire.bot.command.arguments.types.impl.UserArgument;
+
 public interface ArgumentPreset {
+
+    Argument<User> USER = new UserArgument()
+            .name("user")
+            .id("user");
+
+    Argument<Member> MEMBER = new MemberArgument()
+            .name("member")
+            .id("member");
 }
