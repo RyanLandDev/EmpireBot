46889824d866d5ddac640d3ea01afa896f838cee
RyanLandDev <46247385+RyanLandDev@users.noreply.github.com>
2021-08-05 20:30:52 +0200

    Update SubCommand.java

--- src/main/java/net/ryanland/empire/bot/command/impl/SubCommand.java
+++ src/main/java/net/ryanland/empire/bot/command/impl/SubCommand.java
@@ -1,9 +1,4 @@
 package net.ryanland.empire.bot.command.impl;
 
 public abstract class SubCommand extends Command {
-
-    @Override
-    public String getDescription() {
-        throw new IllegalStateException();
-    }
 }
