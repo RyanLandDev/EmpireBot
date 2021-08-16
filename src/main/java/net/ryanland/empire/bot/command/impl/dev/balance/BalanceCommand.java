82a0f9ebb5aa14fcbb2aed035d33d897937925b6
Bence R <bencethegreat@gmail.com>
2021-08-17 00:02:51 +0100

    BalanceCommand improvements

--- src/main/java/net/ryanland/empire/bot/command/impl/dev/balance/BalanceCommand.java
+++ src/main/java/net/ryanland/empire/bot/command/impl/dev/balance/BalanceCommand.java
@@ -3,7 +3,6 @@ package net.ryanland.empire.bot.command.impl.dev.balance;
 import net.ryanland.empire.bot.command.impl.SubCommandHolder;
 import net.ryanland.empire.bot.command.info.Category;
 import net.ryanland.empire.bot.command.info.CommandInfo;
-import net.ryanland.empire.bot.command.info.SubCommandGroup;
 
 public class BalanceCommand extends SubCommandHolder {
     @Override
@@ -12,12 +11,11 @@ public class BalanceCommand extends SubCommandHolder {
                 .name("balance")
                 .description("Modifies a user's balance values.")
                 .category(Category.DEVELOPER)
-                .subCommandGroups(
-                        new SubCommandGroup("actions", "Manages balance actions.",
-                                new BalanceSetCommand(),
-                                new BalanceAddCommand(),
-                                new BalanceSubtractCommand())
-                        );
+                .subCommands(
+                        new BalanceSetCommand(),
+                        new BalanceAddCommand(),
+                        new BalanceSubtractCommand()
+                );
 
     }
 }
--- src/main/java/net/ryanland/empire/bot/command/impl/dev/balance/BalanceSubtractCommand.java
+++ src/main/java/net/ryanland/empire/bot/command/impl/dev/balance/BalanceSubtractCommand.java
@@ -1,5 +1,6 @@
 package net.ryanland.empire.bot.command.impl.dev.balance;
 
+import net.dv8tion.jda.api.entities.User;
 import net.ryanland.empire.bot.command.arguments.ArgumentSet;
 import net.ryanland.empire.bot.command.arguments.types.impl.*;
 import net.ryanland.empire.bot.command.executor.exceptions.CommandException;
@@ -8,6 +9,8 @@ import net.ryanland.empire.bot.command.info.CommandInfo;
 import net.ryanland.empire.bot.events.CommandEvent;
 import net.ryanland.empire.sys.file.database.DocumentCache;
 import net.ryanland.empire.sys.file.database.documents.impl.UserDocument;
+import net.ryanland.empire.sys.message.builders.PresetBuilder;
+import net.ryanland.empire.sys.message.builders.PresetType;
 
 public class BalanceSubtractCommand extends SubCommand {
     @Override
@@ -20,24 +23,33 @@ public class BalanceSubtractCommand extends SubCommand {
     @Override
     public ArgumentSet getArguments() {
         return new ArgumentSet().addArguments(
-                new UserArgument()
-                        .description("User to modify.")
-                        .id("user")
-                        .optional(CommandEvent::getUser),
-                new EnumArgument()
+                new StringArgument()
                         .description("Balance to modify.")
                         .id("balance"),
                 new IntegerArgument()
                         .description("Value to subtract.")
-                        .id("value")
+                        .id("value"),
+                new UserArgument()
+                        .description("User to modify.")
+                        .id("user")
+                        .optional(CommandEvent::getUser)
         );
     }
 
     @Override
     public void run(CommandEvent event) throws CommandException {
-        UserDocument document = DocumentCache.get(event.getArgument("user"), UserDocument.class);
+        User user = event.getArgument("user");
+        UserDocument document = DocumentCache.get(user, UserDocument.class);
         String balance = event.getArgument("balance");
+        int value = event.getArgument("value");
 
-        document.put(balance, document.getInteger(balance) - (int)event.getArgument("value"));
+        document.put(balance, document.getInteger(balance) - value);
+        document.update();
+
+        event.reply(new PresetBuilder(PresetType.SUCCESS)
+                .setDescription(String.format("Successfully subtracted %s from %s' %s balance.",
+                        value, balance, user.getAsMention()))
+                .addLogo()
+        );
     }
 }
